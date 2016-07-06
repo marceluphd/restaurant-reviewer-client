937d202d812f0efa9a191615a28607d27ba94db2
Yuichi Hagio <yhagio87@gmail.com>
2016-07-06 05:43:02 -0400

    designing user redux WIP

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
-
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import Main from './containers/main/mainContainer';
 import Home from './containers/home/homeContainer';
 import Reviews from './containers/reviews/reviewsContainer';
@@ -10,17 +12,26 @@ import Signin from './containers/signin/signinContainer';
 import Signout from './containers/signout/signoutContainer';
 import ReviewForm from './containers/reviewForm/reviewFormContainer';
 
-render(
-  <Router history={hashHistory}>
-    <Route path='/' component={Main}>
-      <IndexRoute component={Reviews} />
-      <Route path='/signup' component={Signup} />
-      <Route path='/signin' component={Signin} />
-      <Route path='/signout' component={Signout} />
-      <Route path='/create-review' component={ReviewForm} />
+import * as reducers from 'redux/modules';
 
-      <Route path="*" component={Home}/>
-    </Route>
-  </Router>,
+const store = createStore(combineReducers(reducers), compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : (f) => f
+));
+
+render(
+  <Provider store={store}>
+    <Router history={hashHistory}>
+      <Route path='/' component={Main}>
+        <IndexRoute component={Reviews} />
+        <Route path='/signup' component={Signup} />
+        <Route path='/signin' component={Signin} />
+        <Route path='/signout' component={Signout} />
+        <Route path='/create-review' component={ReviewForm} />
+
+        <Route path="*" component={Home}/>
+      </Route>
+    </Router>
+  </Provider>,
   document.getElementById('app')
 )
--- src/redux/modules/users.js
+++ src/redux/modules/users.js
@@ -1,1 +1,131 @@
+const ROOT_URL = 'http://localhost:3000';
 
+const AUTH_USER = 'AUTH_USER';
+const UNAUTH_USER = 'UNAUTH_USER';
+const AUTH_ERROR = 'AUTH_ERROR';
+const FETCHING_USER = 'FETCHING_USER';
+const FETCHING_USER_FAILURE = 'FETCHING_USER_FAILURE';
+const FETCHING_USER_SUCCESS = 'FETCHING_USER_SUCCESS';
+
+
+
+function authenticateUser() {
+  return {
+    type: AUTH_USER
+  }
+}
+
+export function authenticationError(error) {
+  return {
+    type: AUTH_ERROR,
+    error
+  };
+};
+
+function fetchingUser() {
+  return {
+    type: FETCHING_USER
+  };
+}
+
+function fetchingUserFailure (error) {
+  return {
+    type: FETCHING_USER_FAILURE,
+    error: 'Error fetching user.'
+  };
+}
+
+function fetchingUserSuccess (user) {
+  return {
+    type: FETCHING_USER_SUCCESS,
+    user
+  };
+}
+
+function unauthUser() {
+  return {
+    type: UNAUTH_USER
+  };
+}
+
+
+
+const initialState = {
+  isFetching: false,
+  error: '',
+  isAuthenticated: false,
+  authedId: '',
+};
+
+export default function users(state = initialState, action) {
+  switch(action.type) {
+
+    case AUTH_USER :
+      return {
+        ...state,
+        isAuthenticated: true,
+        authedId: action.uid
+      };
+
+    case UNAUTH_USER :
+      return {
+        ...state,
+        isAuthenticated: false,
+        authedId: ''
+      };
+
+    case AUTH_ERROR :
+      return {
+        ...state,
+        error: action.error
+      };
+
+    case FETCHING_USER:
+      return {
+        ...state,
+        isFetching: true
+      };
+
+    case FETCHING_USER_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.error
+      };
+
+    case FETCHING_USER_SUCCESS:
+      return action.user === null
+        ? {
+          ...state,
+          isFetching: false,
+          error: ''
+        }
+        : {
+          ...state,
+          isFetching: false,
+          error: '',
+          user: user(null, action)
+        };
+
+    default:
+      return state;
+  }
+}
+
+const initialUserState = {
+  username: '',
+  email: ''
+};
+
+function user (state = initialUserState, action) {
+  switch (action.type) {
+    case FETCHING_USER_SUCCESS :
+      return {
+        ...state,
+        username: action.user.username,
+        email: action.user.email
+      };
+    default :
+      return state;
+  }
+}
