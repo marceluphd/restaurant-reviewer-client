5b8cc3b18072c0e84d49481f9a2381e6da1a79aa
Yuichi Hagio <yhagio87@gmail.com>
2016-07-25 20:33:07 -0400

    Basic styling for reviewForm, RestaurantOne

--- src/components/restaurant/restaurantOne.js
+++ src/components/restaurant/restaurantOne.js
@@ -1,6 +1,22 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 const { object, string, func, bool } = PropTypes;
+import { 
+  restaurantImage,
+  photoBox,
+  restoBox, 
+  starRatings,
+  createReviewButton,
+  openHours,
+  restaurantBox,
+  photoBoxOne,
+  address,
+  reviewsList,
+  reviewItem,
+  reviewComment,
+  reviewAuthor,
+  authorName
+} from './restaurant.css';
 
 RestaurantOne.propTypes = {
   restaurant: object.isRequired
@@ -11,9 +27,17 @@ export default function RestaurantOne(props) {
   function renderReviews(){
     if (props.restaurant.reviews.length > 0) {
       return props.restaurant.reviews.map((r) => {
-        return <div key={r._id}>
-          <p>{r.comment} by {r.author.username}</p>
-        </div>;
+        return (
+          <li key={r._id} className={ reviewItem }>
+            <div className={ reviewComment }>
+              <p>{r.comment}</p>
+            </div>
+            <div className={ reviewAuthor }>
+              <div className={ starRatings } title=".1000"></div>
+              <p className={ authorName }>{r.author.username}</p>
+            </div>
+          </li>
+        );
       });
     }
   }
@@ -23,28 +47,46 @@ export default function RestaurantOne(props) {
     ? <h2 className='header'>{ 'Fetching' }</h2>
     : <div>
         { Object.keys(props.restaurant).length === 0
-          ? <p>{' None '}</p>
+          ? <p>{'N/A'}</p>
           :<div>
-            <h4>{ props.restaurant.name }</h4>
-            <p>Addess: { props.restaurant.address }</p>
-            <p>Open housrs:</p>
-            <div className="open-hours">
-                <p>Mon: { props.restaurant.hours.mon_start }:00 - { props.restaurant.hours.mon_end }:00</p>
-                <p>Tue: { props.restaurant.hours.tue_start }:00 - { props.restaurant.hours.tue_end }:00</p>
-                <p>Wed: { props.restaurant.hours.wed_start }:00 - { props.restaurant.hours.wed_end }:00</p>
-                <p>Thu: { props.restaurant.hours.thu_start }:00 - { props.restaurant.hours.thu_end }:00</p>
-                <p>Fri: { props.restaurant.hours.fri_start }:00 - { props.restaurant.hours.fri_end }:00</p>
-                <p>Sat: { props.restaurant.hours.sat_start }:00 - { props.restaurant.hours.sat_end }:00</p>
-                <p>Sun: { props.restaurant.hours.sun_start }:00 - { props.restaurant.hours.sun_end }:00</p>
+          
+            <h3>{ props.restaurant.name }</h3>
+            
+            <div className={ restaurantBox }>
+              <div className={ photoBoxOne }>
+                <img src={props.restaurant.photo} alt={`Photo of ${props.restaurant.name}`} className={ restaurantImage }/>
+              </div>
+              <div className={ restoBox }>
+                <p><strong>Address:</strong></p>
+                <p className={ address } >{ props.restaurant.address }</p>
+                <p><strong>Open housrs:</strong></p>
+                <div className={ openHours }>
+                  <p>Mon: { props.restaurant.hours.mon_start }:00 - { props.restaurant.hours.mon_end }:00</p>
+                  <p>Tue: { props.restaurant.hours.tue_start }:00 - { props.restaurant.hours.tue_end }:00</p>
+                  <p>Wed: { props.restaurant.hours.wed_start }:00 - { props.restaurant.hours.wed_end }:00</p>
+                  <p>Thu: { props.restaurant.hours.thu_start }:00 - { props.restaurant.hours.thu_end }:00</p>
+                  <p>Fri: { props.restaurant.hours.fri_start }:00 - { props.restaurant.hours.fri_end }:00</p>
+                  <p>Sat: { props.restaurant.hours.sat_start }:00 - { props.restaurant.hours.sat_end }:00</p>
+                  <p>Sun: { props.restaurant.hours.sun_start }:00 - { props.restaurant.hours.sun_end }:00</p>
+                </div>
+
+                <div className={ starRatings } title=".1000"></div>
+                
+                <span> 
+                  {(props.restaurant.total_ratings === 0) 
+                  ? 'N/A' 
+                  : (props.restaurant.total_ratings / props.restaurant.reviews.length)} ({props.restaurant.reviews.length} reviews)
+                </span>
+              </div>
             </div>
-            <p>Reviews: Average 
-              {(props.restaurant.total_ratings === 0) 
-              ? 'None' 
-              : (props.restaurant.total_ratings / props.restaurant.reviews.length)} ({props.restaurant.reviews.length} reviews)
-            </p>
-            <Link to={'/restaurants/' + props.restaurant._id + '/create-review'}>Create Review</Link>
-            <hr />
-            {renderReviews()}
+
+            <br /><br />
+
+            <Link to={'/restaurants/' + props.restaurant._id + '/create-review'} className={ createReviewButton }>Create Review</Link>
+            <ul className={ reviewsList } >
+              <h3>Reviews</h3>
+              {renderReviews()}
+            </ul>
 
           </div>}
       </div>;
--- src/components/reviewForm/reviewForm.js
+++ src/components/reviewForm/reviewForm.js
@@ -1,5 +1,10 @@
 import React, { PropTypes } from 'react';
-import { starabilityBasic } from './style.css';
+import { 
+  starabilityBasic, 
+  labeled,
+  inputField,
+  submitButton
+} from './style.css';
 
 const { object, string, func, bool } = PropTypes;
 
@@ -18,27 +23,35 @@ export default function ReviewForm(props) {
   return (
     <form onSubmit={handleFormSubmit}>
 
-      <input
-        type="text"
-        name="comment"
-        placeholder="comment"
-        onChange={(e) => props.updateComment(e.target.value)}/>
+      <span id="errReviewForm" className="error"></span>
+      <label className={ labeled }>Review Comment<br />
+        <input
+          id="comment"
+          name="comment"
+          placeholder="comment"
+          onChange={(e) => props.updateComment(e.target.value)}
+          className={ inputField }
+          type="text"
+          required />
+      </label>
 
       <fieldset className={starabilityBasic}>
         <legend>Rate this restaurant:</legend>
-        <input type="radio" id="first-rate5" name="rating" value="5" onChange={(e) => props.updateComment(e.target.value)} />
+        <input type="radio" id="first-rate5" name="rating" value="5" onChange={(e) => props.updateRating(e.target.value)} />
         <label htmlFor="first-rate5" title="Amazing">5 stars</label>
-        <input type="radio" id="first-rate4" name="rating" value="4" onChange={(e) => props.updateComment(e.target.value)} />
+        <input type="radio" id="first-rate4" name="rating" value="4" onChange={(e) => props.updateRating(e.target.value)} />
         <label htmlFor="first-rate4" title="Very good">4 stars</label>
-        <input type="radio" id="first-rate3" name="rating" value="3" onChange={(e) => props.updateComment(e.target.value)} />
+        <input type="radio" id="first-rate3" name="rating" value="3" onChange={(e) => props.updateRating(e.target.value)} />
         <label htmlFor="first-rate3" title="Average">3 stars</label>
-        <input type="radio" id="first-rate2" name="rating" value="2" onChange={(e) => props.updateComment(e.target.value)} />
+        <input type="radio" id="first-rate2" name="rating" value="2" onChange={(e) => props.updateRating(e.target.value)} />
         <label htmlFor="first-rate2" title="Not good">2 stars</label>
-        <input type="radio" id="first-rate1" name="rating" value="1" onChange={(e) => props.updateComment(e.target.value)} />
+        <input type="radio" id="first-rate1" name="rating" value="1" onChange={(e) => props.updateRating(e.target.value)} />
         <label htmlFor="first-rate1" title="Terrible">1 star</label>
       </fieldset>
 
-      <button>Submit</button>
+      <button 
+        action="submit"
+        className={ submitButton }>Submit</button>
     </form>
   );
 };
