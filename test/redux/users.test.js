6bfbfd972207d9157fd8bef9dcba74dd196a2a69
Yuichi Hagio <yhagio87@gmail.com>
2016-08-31 22:11:31 -0400

    testing setup

---
+++ test/redux/users.test.js
@@ -0,0 +1,11 @@
+import { Map } from 'immutable';
+import { expect } from 'chai';
+import { spy } from 'sinon';
+import nock from 'nock';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+// import * as usersRedux from '../../src/redux/modules/users';
+
+describe('[Redux] users actions', () => {
+  it('')
+});
---
+++ test/setup.js
@@ -0,0 +1,26 @@
+const noop = () => {};
+
+require.extensions['.css'] = noop;
+require.extensions['.sass'] = noop;
+require.extensions['.ico'] = noop;
+require.extensions['.jpg'] = noop;
+require.extensions['.jpeg'] = noop;
+require.extensions['.png'] = noop;
+require.extensions['.svg'] = noop;
+
+var jsdom = require('jsdom').jsdom;
+
+var exposedProperties = ['window', 'navigator', 'document'];
+
+global.document = jsdom('');
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
+});
+
+global.navigator = {
+  userAgent: 'node.js'
+};
